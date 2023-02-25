372bb079b5745be9060b52d5a885692da6b428dd
luffy <ilyagt@gmail.com>
2023-02-25 14:40:59 +1000

    init commit

---
+++ src/main/java/ru/trinko/springcourse/config/SpringConfig.java
@@ -0,0 +1,12 @@
+package ru.trinko.springcourse.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("ru.trinko.springcourse")
+@EnableWebMvc
+public class SpringConfig {
+
+}
